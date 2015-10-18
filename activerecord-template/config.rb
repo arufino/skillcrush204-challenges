161587f7e9344385abc290be338955d03c17ac5c
Kibou <a.rufino@altraduct.com>
2015-10-18 11:08:12 +0200

    Activerecord practice

--- activerecord-template/config.rb
+++ activerecord-template/config.rb
@@ -1,8 +1,7 @@
-require "pathname"
-require "pg"
-require "active_record"
-require "logger"
-require "bigdecimal"
+require 'pathname'
+require 'pg'
+require 'active_record'
+require 'logger'
 
 ## Load all files and configure the db
 
@@ -10,30 +9,28 @@ APP_ROOT = Pathname.new(File.expand_path(File.dirname(__FILE__)))
 
 APP_NAME = APP_ROOT.basename.to_s
 
-DB_PATH  = APP_ROOT.join("db", APP_NAME + "_development.db").to_s
+DB_PATH  = APP_ROOT.join('db', APP_NAME + "_development.db").to_s
 
 DB_NAME = APP_NAME + "_development.db"
 
 TEST_DB_NAME = APP_NAME + "_test.db"
 
-DB_USERNAME = "alicia"
+DB_USERNAME = 'postgres'
 
-DB_PASSWORD = "kibou"
+DB_PASSWORD = 'kibou'
 
-
-
-if ENV["DEBUG"]
+if ENV['DEBUG']
   ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
 
 
-Dir[APP_ROOT.join("models", "*.rb")].each do |model_file|
-  filename = File.basename(model_file).gsub(".rb", "")
+Dir[APP_ROOT.join('models', '*.rb')].each do |model_file|
+  filename = File.basename(model_file).gsub('.rb', '')
   autoload ActiveSupport::Inflector.camelize(filename), model_file
 end
 
-ActiveRecord::Base.establish_connection :adapter  => "postgresql",
+ActiveRecord::Base.establish_connection :adapter  => 'postgresql',
                                         :database => DB_NAME,
-                                        :host => "localhost",
+                                        :host => 'localhost',
                                         :username => DB_USERNAME,
                                         :password => DB_PASSWORD
